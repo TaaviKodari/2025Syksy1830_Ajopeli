8c6198ac9e63b8bc59834a60fe1a9a706a9d3336
Your Name <you@example.com>
2025-10-01 19:32:26 +0300

    waypoint ja vastustaja menee niitä pitkin

--- 2025SyksyKE1830_Ajopeli/Assets/OmatAssetit/Scripts/AICar.cs
+++ 2025SyksyKE1830_Ajopeli/Assets/OmatAssetit/Scripts/AICar.cs
@@ -3,15 +3,26 @@ using UnityEngine;
 public class AICar : MonoBehaviour
 {
     public Transform[] waypoints;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    private int currentWaypointIndex = 0;
+    public float speed = 10f;
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        Transform target = waypoints[currentWaypointIndex];
+        Vector3 targetXZ = new Vector3(target.position.x, transform.position.y, target.position.z);
+        Vector3 direction = (targetXZ - transform.position).normalized;
+
+        Quaternion lookRotation = Quaternion.LookRotation(direction);
+
+        transform.rotation = lookRotation;
+
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, targetXZ) < 2f)
+        {
+            currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
+        }
     }
 }
