1780ab3946c630a2c733bd427965b528876a6c86
Your Name <you@example.com>
2025-11-12 19:31:00 +0200

    yksi voittoja

--- 2025SyksyKE1830_Ajopeli/Assets/OmatAssetit/Scripts/Tuomari.cs
+++ 2025SyksyKE1830_Ajopeli/Assets/OmatAssetit/Scripts/Tuomari.cs
@@ -2,11 +2,33 @@ using UnityEngine;
 
 public class Tuomari : MonoBehaviour
 {
+    private bool winnerDeclared = false;
+        
     private void OnTriggerEnter(Collider car)
     {
         CarIdentify id = car.GetComponent<CarIdentify>();
 
         string winnerName = id.displayName;
-        Debug.Log($"WINNER: {winnerName}");
+        
+        if(id.kind == CarKind.Player)
+        {
+            var validator = car.GetComponent<PelaajanKierrostarkastus>();
+            if (validator == null)
+            {
+                Debug.LogError("Puuttuu PelaajanKierrostarkastus scripti");
+                return;
+            }
+
+            if (!validator.AllVisitedThisLap)
+            {
+                Debug.Log("Pelaaja ylitti maaliviivan, mutta kaikki checkpointit eivät ole kunnossa -> ei voittoa!");
+                return;
+            }
+        }
+        if(winnerDeclared == false)
+        {
+            winnerDeclared = true;
+            Debug.Log($"WINNER: {winnerName}");
+        }
     }
 }
